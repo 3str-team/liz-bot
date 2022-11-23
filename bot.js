498283c6f02b3471d556e9bbcd35c0e62f7ea26b
3str <ssrghome@gmail.com>
2022-11-23 19:02:41 +0300

    deploy

--- bot.js
+++ bot.js
@@ -1,19 +1,24 @@
 require("dotenv").config();
 
+let ip = require("ip");
+
 const TelegramApi = require("node-telegram-bot-api");
 
 const bot = new TelegramApi(process.env.TELEGRAM_API_TOKEN, { polling: true });
 
-const chatId = process.env.LIZKA_ID;
+let chatId = process.env.LIZKA_ID;
 
 bot.on("message", (msg) => {
-  console.log(msg.text);
-  bot.sendMessage(msg.chat.id, msg.text);
+  if (msg.chat.username != "eliz_sharapova") {
+    bot.sendMessage(msg.chat.id, ip.address());
+  }
 });
 
-// setInterval(() => {
-//   bot.sendMessage(
-//     chatId,
-//     "Лиза, привет, это Серёжа. Пишу просто напомнить, что я тебя люблю)"
-//   );
-// }, 1000);
+setInterval(() => {
+  if (chatId) {
+    bot.sendMessage(
+      chatId,
+      "Лиза, привет, это Серёжа. Пишу просто напомнить, что я тебя люблю)"
+    );
+  }
+}, 10000);
